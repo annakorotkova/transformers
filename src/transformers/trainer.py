fbd85a61295215edb17cea638f96b1b27f103b76
annakorotkova <44239778+annakorotkova@users.noreply.github.com>
2020-07-17 18:37:42 +0200

    Update trainer.py
    # add locals() to globals()
            globals()['local'] = locals()

--- src/transformers/trainer.py
+++ src/transformers/trainer.py
@@ -582,7 +582,9 @@ class Trainer:
             # Clean the state at the end of training
             delattr(self, "_past");
         '''
-        self.finetuning_time_list = timeit.Timer(stmt = finetuning_statement, globals = {'local': locals().items(), 'global': globals().items()}).repeat(repeat = 1, number = 2)  # pass both global and local variables
+        # add locals() to globals()
+        globals()['local'] = locals()
+        self.finetuning_time_list = timeit.Timer(stmt = finetuning_statement, globals = globals()).repeat(repeat = 1, number = 2)  # pass both global and local variables
         
         self.finetuning_time = min(self.finetuning_time_list) / self.args.train_time_number
             
