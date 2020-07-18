f3ee2b7a7ad734a2fa22c4d4f3641374c193078c
annakorotkova <44239778+annakorotkova@users.noreply.github.com>
2020-07-18 14:34:01 +0200

    Update training_args.py
    added finetuning_iters (-> changed logic in train.py; now: execute for loop to finetune model in order to receive a more stable value for the finetuning time; timeit.repeat() was too complicated (needs locals() and globals())

--- src/transformers/training_args.py
+++ src/transformers/training_args.py
@@ -109,10 +109,8 @@ class TrainingArguments:
             make use of the past hidden states for their predictions. If this argument is set to a positive int, the
             ``Trainer`` will use the corresponding output (usually index 2) as the past state and feed it to the model
             at the next training step under the keyword argument ``mems``.
-        train_time_repeat (:obj:`float`, `optional`, defaults to 3):
-            Number of repeat count within timeit.repeat() function for measuring the finetuning_time (refers to argument 'repeat' in timeit.repeat()).
-        train_time_number (:obj:`float`, `optional`, defaults to 1):
-            Number executions within timeit.repeat() function for measuring the finetuning_time (refers to argument 'number' in timeit.repeat()).
+        finetuning_iters (:obj:`float`, `optional`, defaults to 3):
+            Number of iterations of for loop for finetuning; in order to receive a more stable value for the finetuning time.
     """
 
     # train_time_repeat and train_time_number added by Anna
@@ -225,17 +223,11 @@ class TrainingArguments:
         default=-1,
         metadata={"help": "If >=0, uses the corresponding part of the output as the past state for next step."},
     )
-    
+        
     # Added by Anna
-    train_time_repeat: float = field(
+    finetuning_iters: float = field(
         default=3,
-        metadata={"help": "Repeat timeit.repeat() function for measuring finetuning_time 'train_time_repeat' times."},
-    )
-    
-    # Added by Anna
-    train_time_number: float = field(
-        default=1,
-        metadata={"help": "Execute timeit.repeat() function for measuring finetuning_time 'train_time_number' number of times."},
+        metadata={"help": "Execute finetuning 'finetuning_iters' times for measuring finetuning_time."},
     )
 
     @property
